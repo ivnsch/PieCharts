2d0f451334df68efcec19672a557264829eb4717
garam <studio@gar.am>
2017-02-07 18:40:48 +0900

    Add property ‘name’ in PieSliceModel

--- PieCharts/Model/PieSliceModel.swift
+++ PieCharts/Model/PieSliceModel.swift
@@ -10,10 +10,12 @@ import UIKit
 
 public class PieSliceModel: CustomDebugStringConvertible {
     
+    public let name: String?
     public let value: Double
     public let color: UIColor
     
-    public init(value: Double, color: UIColor) {
+    public init(name: String? = nil, value: Double, color: UIColor) {
+        self.name = name
         self.value = value
         self.color = color
     }
