62198ed649c3c5d59569141e6e9651b73ff24a68
garam <studio@gar.am>
2017-02-07 18:42:03 +0900

    Add property related in AttributedString to support label with attributed string

--- PieCharts/Layer/Impl/Common/PieChartLabelSettings.swift
+++ PieCharts/Layer/Impl/Common/PieChartLabelSettings.swift
@@ -9,12 +9,19 @@
 import UIKit
 
 public class PieChartLabelSettings {
+    // String type checker - check type to use attributed string
+    public enum `Type` {
+        case plain
+        case attributed
+    }
     
+    public var type: Type = .plain
     public var textColor: UIColor = UIColor.black
     public var bgColor: UIColor = UIColor.clear
     public var font: UIFont = UIFont.boldSystemFont(ofSize: 20)
     
-    public var textGenerator: (PieSlice) -> String = {"\($0.data.model.value)"}
+    public var textGenerator: (PieSlice) -> String = { "\($0.data.model.value)" }
+    public var attributedTextGenerator: (PieSlice) -> NSAttributedString = { NSAttributedString(string: "\($0.data.model.value)") }
     
     // Optional custom label - when this is set presentations settings (textColor, etc.) are ignored
     public var labelGenerator: ((PieSlice) -> UILabel)?
