c850e878f3915bb69f564adef396ba7beac8e95f
Ivan Schuetz <ivanschuetz@gmail.com>
2017-05-03 21:34:12 +0200

    Allow to pass objects around with slice model

--- PieCharts/Model/PieSliceModel.swift
+++ PieCharts/Model/PieSliceModel.swift
@@ -12,13 +12,15 @@ public class PieSliceModel: CustomDebugStringConvertible {
     
     public let value: Double
     public let color: UIColor
-    
-    public init(value: Double, color: UIColor) {
+    public let obj: Any? /// optional object to pass around e.g. to the layer's text generators
+
+    public init(value: Double, color: UIColor, obj: Any? = nil) {
         self.value = value
         self.color = color
+        self.obj = obj
     }
     
     public var debugDescription: String {
-        return "{value: \(value)}"
+        return "{value: \(value), obj: \(String(describing: obj))}"
     }
 }
